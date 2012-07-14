3ef22dea0ab53a79f8b852fdc5c8bee5000939fa
Dmitri Molokov <d@moloko.ws>
2012-07-14 11:05:51 +0400

    Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Searchabook::Application.config.secret_token = '5bcb41de70598e411dfdc296d52f516b6e291466c8f283ed63660676c11fffbc6c9695ecb041c8644f0a563e84ab5f97e8de6705bb06ba97889b4dc4f5b94974'
